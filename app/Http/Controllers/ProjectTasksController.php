9c455cf8ca0c9e372d290a21972acaa960040630
Chris XIONG <chrisxiongyj@hotmail.com>
2019-10-22 17:03:15 +0800

    E19

--- app/Http/Controllers/ProjectTasksController.php
+++ app/Http/Controllers/ProjectTasksController.php
@@ -12,18 +12,23 @@ class ProjectTasksController extends Controller
     //
     public function store(Project $project)
     {
-    	$attributes = request()->validate(['description' => 'required']);
+    	// $attributes = request()->validate(['description' => 'required']);
 
-    	$project->addTask($attributes);
-
+    	// $project->addTask($attributes);
+        $project ->addTask(
+            request()->validate(['description' => 'required'])
+        );
     	return back();
+
     }
     public function update(Task $task)
     {
-    	$task->update([
-    		'completed' => request()->has('completed')
-    	]);
-
+        // $task->complete();
+        // $task->complete(request()->has('completed'));
+    	// $task->update([
+    	// 	'completed' => request()->has('completed')
+    	// ]);
+        request()->has('completed') ? $task->complete() : $task->incomplete();
     	return back();
     }
 }
